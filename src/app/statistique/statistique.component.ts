b9bf47132e41ccb43f74139f97dc2b54e654c0a8
Clara <goncalvesclara.pro@gmail.com>
2022-01-25 10:51:53 +0100

    Exercice 8

--- src/app/statistique/statistique.component.ts
+++ src/app/statistique/statistique.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Statistique } from '../models/statistique';
+import { StatistiqueService } from '../service/statistique.service';
 
 @Component({
   selector: 'app-statistique',
@@ -9,9 +10,17 @@ import { Statistique } from '../models/statistique';
 export class StatistiqueComponent implements OnInit {
   @Input() stat!: Statistique;
 
-  constructor() { }
+  constructor(private statServ: StatistiqueService) { }
 
   ngOnInit(): void {
+  }
+
+  deleteStat(delStatId: string) {
+    for (let i = 0; i < this.statServ.stats.length; i++) {
+      if (this.statServ.stats[i].id = delStatId) {
+        this.statServ.stats.splice(i, 1);
+      }
+    }
   }
 
 }
