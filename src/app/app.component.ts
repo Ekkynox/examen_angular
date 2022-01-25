65273dbe7b3f47317574bbe03bdab7a5b7beb358
Clara <goncalvesclara.pro@gmail.com>
2022-01-25 10:23:30 +0100

    Exercice 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'examen_angular';
+  stat1: Statistique = { id: "dgkdjgc98547gj5", title: "Âge moyen des hommes en France", value: 40.8 };
+  stat2: Statistique = { id: "jgj6894gjg6", title: "Âge moyen des femmes en France", value: 43.6 };
 }
