6e1970d5a8a74d9787bd7e89fdf411e200d32474
Clara <goncalvesclara.pro@gmail.com>
2022-01-25 10:37:45 +0100

    Exercice 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,12 @@ import { Statistique } from './models/statistique';
 
 export class AppComponent {
   title = 'examen_angular';
-  stat1: Statistique = { id: "dgkdjgc98547gj5", title: "Âge moyen des hommes en France", value: 40.8 };
-  stat2: Statistique = { id: "jgj6894gjg6", title: "Âge moyen des femmes en France", value: 43.6 };
+  stats: Statistique[] = [
+    { id: "dgkdjgc98547gj5", title: "Âge moyen des hommes en France", value: 40.8 },
+    { id: "jgj6894gjg6", title: "Âge moyen des femmes en France", value: 43.6 }
+  ];
+
+  to = setTimeout(() => {
+    this.stats.push({ id: "kgjdkgj879khj", title: "Naissance en France en 2019", value: 753000 })
+  }, 1000);
 }
