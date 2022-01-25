4caf795626c9ac0581e185ab5685703f83f9251a
Clara <goncalvesclara.pro@gmail.com>
2022-01-25 11:52:45 +0100

    Exercice 12

--- src/app/ajout/ajout.component.ts
+++ src/app/ajout/ajout.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Statistique } from '../models/statistique';
 import { StatistiqueService } from '../service/statistique.service';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-ajout',
@@ -13,14 +15,22 @@ export class AjoutComponent implements OnInit {
   title!: string;
   value!: number;
 
-  constructor(private statServ: StatistiqueService) { }
+  constructor(private statServ: StatistiqueService, private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   addStat(form: NgForm) {
-    this.statServ.stats.push(new Statistique(this.id, this.title, this.value));
+    let stat = new Statistique(this.id, this.title, this.value);
+    this.statServ.stats.push(stat);
     form.reset();
+
+    lastValueFrom(this.http.post<Statistique>("https://stats.naminilamy.fr/", JSON.stringify(stat)))
+      .then(res => {
+        console.log("ok");
+      }, error => {
+        console.log("pas ok")
+      })
   }
 
 }
